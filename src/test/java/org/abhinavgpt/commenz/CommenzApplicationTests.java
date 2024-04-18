47116df80d4d6e1e5610d8c074c03f5082b96def
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-04-18 23:51:07 +0530

    v0 Amazon Scrapper
    * Created and tested basic amazon scrapper
    
    * Removed redundant test file, started tracking readme file
    
    * Removed redundant code, comments and lines
    
    * Added sealed interfaces removed public class

--- src/test/java/org/abhinavgpt/commenz/CommenzApplicationTests.java
+++ src/test/java/org/abhinavgpt/commenz/CommenzApplicationTests.java
@@ -1,13 +1,32 @@
 package org.abhinavgpt.commenz;
 
+import org.abhinavgpt.commenz.services.scrapping.Scrapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CommenzApplicationTests {
 
+    @Autowired
+    Scrapper scrapper;
+
     @Test
-    void contextLoads() {
+    void testCleaningURL() {
+        String url = "https://www.amazon.in/Apple-iPhone-11-64GB-Black/dp/B07XVMJF2D/ref=sr_1_1?dchild=1&keywords=iphone+11&qid=1616820734&sr=8-1";
+        String cleanedURL = scrapper.cleaningURL(url);
+        assertEquals("https://www.amazon.in/product-reviews/B07XVMJF2D", cleanedURL);
+
     }
 
+    @Test
+    void testScrap() {
+        String url = "https://www.amazon.in/kalsdg;oiuajkjassdf/product-reviews/B0B5S2ZJ8H/ref=cm_cr_arp_d_viewopt_srt?ie=UTF8";
+        var reviews = scrapper.scrap(url);
+        System.out.println(reviews);
+        assertEquals(10, reviews.size());
+    }
 }
