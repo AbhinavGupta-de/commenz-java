f784585699e9b5ece57305c11f718a01461aad9a
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-04-18 20:28:44 +0530

    Removed redundant code, comments and lines

--- src/test/java/org/abhinavgpt/commenz/CommenzApplicationTests.java
+++ src/test/java/org/abhinavgpt/commenz/CommenzApplicationTests.java
@@ -14,16 +14,14 @@ class CommenzApplicationTests {
     @InjectMocks
     Scrapper scrapper = new AmazonScrapper();
 
-//     Test the newly made cleaning url function
     @Test
     void testCleaningURL() {
-        String url = "https://www.amazon.in/Apple-iPhone-11-64GB-Black/dp//ref=sr_1_1?dchild=1&keywords=iphone+11&qid=1616820734&sr=8-1";
+        String url = "https://www.amazon.in/Apple-iPhone-11-64GB-Black/dp/B07XVMJF2D/ref=sr_1_1?dchild=1&keywords=iphone+11&qid=1616820734&sr=8-1";
         String cleanedURL = scrapper.cleaningURL(url);
         assertEquals("https://www.amazon.in/product-reviews/B07XVMJF2D", cleanedURL);
 
     }
 
-//     Test the newly made scrap function
     @Test
     void testScrap() {
         String url = "https://www.amazon.in/kalsdg;oiuajkjassdf/product-reviews/B0B5S2ZJ8H/ref=cm_cr_arp_d_viewopt_srt?ie=UTF8";
